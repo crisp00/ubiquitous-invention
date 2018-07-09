b9ea6f881732fc6d6d2e5066cda0c6c487698d27
Cris Pintea <cris@pintea.net>
2018-07-09 23:42:05 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+var libQ = require('kew');
+module.exports = ExamplePlugin;
+
+function ExamplePlugin(context) {
+  var self = this;
+  self.context = context;
+  self.commandRouter = this.context.coreCommand;
+}
+
+
+/*
+ * This method can be defined by every plugin which needs to be informed of the startup of Volumio.
+ * The Core controller checks if the method is defined and executes it on startup if it exists.
+ */
+ExamplePlugin.prototype.onVolumioStart = function () {
+  var self = this;
+  //Perform startup tasks here
+  setTimeout(()=>{
+    self.commandRouter.pushToastMessage('info', "RAAT", "Plugin loaded!");
+  }, 5000);
+  return libQ.resolve();
+};
+
+ExamplePlugin.prototype.onStop = function () {
+  var self = this;
+  //Perform stop tasks here
+  return libQ.resolve();
+};
+
+ExamplePlugin.prototype.onRestart = function () {
+  var self = this;
+  //Perform restart tasks here
+};
+
+ExamplePlugin.prototype.onInstall = function () {
+  var self = this;
+  //Perform your installation tasks here
+};
+
+ExamplePlugin.prototype.onUninstall = function () {
+  var self = this;
+  //Perform your deinstallation tasks here
+};
+
+ExamplePlugin.prototype.getUIConfig = function () {
+  var self = this;
+
+  return {success: true, plugin: "example_plugin"};
+};
+
+ExamplePlugin.prototype.setUIConfig = function (data) {
+  var self = this;
+  //Perform your UI configuration tasks here
+};
+
+ExamplePlugin.prototype.getConf = function (varName) {
+  var self = this;
+  //Perform your tasks to fetch config data here
+};
+
+ExamplePlugin.prototype.setConf = function (varName, varValue) {
+  var self = this;
+  //Perform your tasks to set config data here
+};
+
+//Optional functions exposed for making development easier and more clear
+ExamplePlugin.prototype.getSystemConf = function (pluginName, varName) {
+  var self = this;
+  //Perform your tasks to fetch system config data here
+};
+
+ExamplePlugin.prototype.setSystemConf = function (pluginName, varName) {
+  var self = this;
+  //Perform your tasks to set system config data here
+};
+
+ExamplePlugin.prototype.getAdditionalConf = function () {
+  var self = this;
+  //Perform your tasks to fetch additional config data here
+};
+
+ExamplePlugin.prototype.setAdditionalConf = function () {
+  var self = this;
+  //Perform your tasks to set additional config data here
+};
